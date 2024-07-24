abf6bae677564985c28078a8f0ca624ce4edacab
dejosemario <josemariaodumodu@gmail.com>
2024-07-24 23:35:34 +0100

    feat: Add environment-specific MongoDB connection configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,33 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const connectDB = async (MONGODB_URI: string): Promise<void> => {
+interface Config {
+  [key: string]: {
+    MONGODB_URI: string | undefined;
+  };
+}
+
+const config: Config = {
+  production: {
+    MONGODB_URI: process.env.PRODUCTION_MONGODB_URI,
+  },
+  staging: {
+    MONGODB_URI: process.env.STAGING_MONGODB_URI,
+  },
+  development: {
+    MONGODB_URI: process.env.DEVELOPMENT_MONGODB_URI,
+  },
+};
+
+const getMongoUrl = async (env: string = "development"): Promise<string> => {
+  return config[env]?.MONGODB_URI as string; 
+};
+
+const connectDB = async () => {
+  const env = process.env.NODE_ENV || "development";
+  const MONGODB_URI = await getMongoUrl(env);
+
   if (!MONGODB_URI) {
     console.log("MONGODB_URI not provided");
     process.exit(1); //exit the process
@@ -14,3 +41,4 @@ export const connectDB = async (MONGODB_URI: string): Promise<void> => {
   }
 };
 
+export { connectDB, getMongoUrl };
