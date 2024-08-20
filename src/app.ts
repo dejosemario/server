bf647cf8c2fcd323a22cae6718e3551d8eb9ed24
dejosemario <josemariaodumodu@gmail.com>
2024-08-20 20:39:21 +0100

    fix: debug memory leak er

--- src/app.ts
+++ src/app.ts
@@ -47,11 +47,11 @@ class EventfulApp {
 
     const limiter = rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 3, // limit each IP to 100 requests per windowMs
+      max: 150, // limit each IP to 100 requests per windowMs
       message:
         "Too many requests from this IP, please try again after 15 minutes",
     });
-    // this.app.use(limiter);
+    this.app.use(limiter);
     this.app.use(morgan("dev"));
 
   }
