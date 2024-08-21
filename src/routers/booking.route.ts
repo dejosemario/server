d6a8ff73da20cca97ef60007cc3e0fba49e36dad
dejosemario <josemariaodumodu@gmail.com>
2024-08-21 14:26:59 +0100

    fix: sort debug error

--- src/routers/booking.route.ts
+++ src/routers/booking.route.ts
@@ -1,49 +1,57 @@
-import express from 'express';
-import {wrapper} from "../utils";
-import isAuthenticated, { cacheMiddleware, isAuthUser } from '../middlewares/auth';
-import {BookingController} from '../controllers/booking.controller';
+import express from "express";
+import { wrapper } from "../utils";
+import isAuthenticated, {
+  cacheMiddleware,
+  isAuthUser,
+} from "../middlewares/auth";
+import { BookingController } from "../controllers/booking.controller";
 
-class BookingRoutes{
+class BookingRoutes {
+  router = express.Router();
+  BookingController = new BookingController();
+  path = "/bookings";
+  constructor() {
+    this.initializeRoutes();
+  }
 
-    router = express.Router();
-    BookingController = new BookingController();
-    path= "/bookings";
-    constructor(){
-        this.initializeRoutes();
-    }
-
-    private initializeRoutes(){
-        this.router.post(
-            `${this.path}/create-booking`,
-            isAuthUser,
-            wrapper(this.BookingController.createBookings.bind(this.BookingController))
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}/create-booking`,
+      isAuthUser,
+      wrapper(
+        this.BookingController.createBookings.bind(this.BookingController)
+      )
+    );
+    this.router.get(
+      `${this.path}/get-user-bookings`,
+      isAuthUser,
+      wrapper(
+        this.BookingController.getUserBookings.bind(this.BookingController)
+      )
+    );
+    this.router.get(
+      `${this.path}/get-all-bookings`,
+      cacheMiddleware,
+      isAuthenticated,
+      wrapper(
+        this.BookingController.getAllBookings.bind(this.BookingController)
+      )
+    ),
+      this.router.post(
+        `${this.path}/cancel-booking`,
+        isAuthenticated,
+        wrapper(
+          this.BookingController.getUserBookings.bind(this.BookingController)
         )
-        this.router.get(
-            `${this.path}/get-user-bookings`,
-            isAuthUser,
-            wrapper(this.BookingController.getUserBookings.bind(this.BookingController))
+      ),
+      this.router.post(
+        `${this.path}/generate-qrcode`,
+        isAuthenticated,
+        wrapper(
+          this.BookingController.generateQRCode.bind(this.BookingController)
         )
-        this.router.get(
-            `${this.path}/get-all-bookings`,
-            cacheMiddleware,
-            isAuthenticated,
-            wrapper(this.BookingController.getAllBookings.bind(this.BookingController))
-        ),
-        this.router.post(
-            `${this.path}/cancel-booking`,
-            isAuthenticated,
-            wrapper(this.BookingController.getUserBookings.bind(this.BookingController))
-        ),
-        this.router.post(
-            `${this.path}/generate-qrcode`,
-            isAuthenticated,
-            wrapper(this.BookingController.generateQRCode.bind(this.BookingController
-        )
-        
-        
-    }
-
-
+      );
+  }
 }
 
 const bookingRoutes = new BookingRoutes();
