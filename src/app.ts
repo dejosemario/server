f70f378e4ab5ba46f830f0f59fae8d2f7b0d2cbd
dejosemario <josemariaodumodu@gmail.com>
2024-08-23 18:02:10 +0100

    fix: handling cors errors

--- src/app.ts
+++ src/app.ts
@@ -31,27 +31,17 @@ class EventfulApp {
 
   private initializeMiddlewares() {
     // Enable Cross Origin Resource Sharing
+   this.app.set("trust proxy", 1);
     const corsOptions = {
       origin: process.env.FRONTEND_URL,
       credentials: true,
       optionsSuccessStatus: 200,
     };
-    this.app.set("trust proxy", true); // Enable trust proxy
 
-    // Handle preflight requests
-    this.app.options("*", cors(corsOptions));
-    
     this.app.use(cors(corsOptions));
-    this.app.use((req: Request, res: Response, next: NextFunction) => {
-      res.header("Access-Control-Allow-Credentials", "true");
-      res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "");
-      res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-      );
-      next();
-    });
+    // Handle preflight requests
+    // this.app.options("*", cors(corsOptions));
+
     this.app.use(express.json());
     this.app.use(cookieParser());
     this.app.use(express.urlencoded({ extended: true }));
